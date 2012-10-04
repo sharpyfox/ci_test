036d6849e03296d24a68ce64f6b590f13a4e3fba
Vasiliev Nikita <sharpyfox@gmail.com>
2012-10-04 23:15:39 +0400

    test

--- test/ci/before_script.sh
+++ test/ci/before_script.sh
@@ -9,43 +9,43 @@ git checkout $REDMINE_GIT_TAG
 git submodule update --init --recursive
 
 # Copy over the already downloaded plugin 
-#cp -r ~/builds/*/$REPO_NAME vendor/plugins/$PLUGIN_DIR
+cp -r ~/builds/*/$REPO_NAME vendor/plugins/$PLUGIN_DIR
 
 #export BUNDLE_GEMFILE=$TARGET_DIR/Gemfile
 
-#bundle install --without=$BUNDLE_WITHOUT
+bundle install --without=$BUNDLE_WITHOUT
 
-#echo "creating $DB database"
-#case $DB in
-  #"mysql" )
-    #mysql -e 'create database redmine_test;'
-    #cat > config/database.yml << EOF
-#test:
-  #adapter: mysql
-  #database: redmine_test
-  #username: postgres
-#EOF
-    #;;
-  #"mysql2" )
-    #mysql -e 'create database redmine_test;'
-    #cat > config/database.yml << EOF
-#test:
-  #adapter: mysql2
-  #username: root
-  #encoding: utf8
-  #database: redmine_test
-#EOF
-    #;;
-  #"postgres" )
-    #psql -c 'create database redmine_test;' -U postgres
-    #cat > config/database.yml << EOF
-#test:
-  #adapter: postgresql
-  #database: redmine_test
-  #username: postgres
-#EOF
-    #;;
-#esac
-#
-#bundle exec rake db:migrate
-#bundle exec rake db:migrate:plugins
+echo "creating $DB database"
+case $DB in
+  "mysql" )
+    mysql -e 'create database redmine_test;'
+    cat > config/database.yml << EOF
+test:
+  adapter: mysql
+  database: redmine_test
+  username: postgres
+EOF
+    ;;
+  "mysql2" )
+    mysql -e 'create database redmine_test;'
+    cat > config/database.yml << EOF
+test:
+  adapter: mysql2
+  username: root
+  encoding: utf8
+  database: redmine_test
+EOF
+    ;;
+  "postgres" )
+    psql -c 'create database redmine_test;' -U postgres
+    cat > config/database.yml << EOF
+test:
+  adapter: postgresql
+  database: redmine_test
+  username: postgres
+EOF
+    ;;
+esac
+
+bundle exec rake db:migrate
+bundle exec rake db:migrate:plugins
